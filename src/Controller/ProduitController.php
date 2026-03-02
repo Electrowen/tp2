1e938b8e23d8d3b4759419a78fa81222e444e700
Electrowen <coconut8654@gmail.com>
2026-03-02 17:48:00 +0100

    TP 3 début 8

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProduitController extends AbstractController
+{
+    #[Route('/produit', name: 'app_produit')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/ProduitController.php',
+        ]);
+    }
+
+    
+}
