2ba27b88d1ee4aa2714d6d78ee6740989327c1c0
Electrowen <coconut8654@gmail.com>
2026-03-12 10:47:38 +0100

    TP4 fini

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,11 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class AccueilController extends AbstractController
+class AccueilController extends AbstractController
 {
     #[Route('/', name: 'accueil_index')]
     public function indexAction(): Response
     {
-        return $this->render('/Accueil/welcome.html.twig');
+        return $this->render('Accueil/index.html.twig');
+    }
+
+    // pour inclusion de contrôleur dans le template secondaire : action non routable
+    public function menuAction(): Response
+    {
+        $args = array(
+        );
+        return $this->render('Layouts/_menu.html.twig', $args);
     }
 }
